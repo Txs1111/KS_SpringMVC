7eb99d514c7a85d5224734491f9aab43af967968
Txs1111 <1831069264@qq.com>
2021-04-22 20:06:54 +0800

    编码问题，控制器跳转，初步了解跳转

---
+++ SpringMVC/out/artifacts/springmvc_04_controller_war_exploded/form.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Lyon
+  Date: 2021-4-22
+  Time: 下午 07:17
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>表单</title>
+</head>
+<body>
+<form action="/add/1/2" method="post">
+    <input type="submit">
+</form>
+</body>
+</html>
---
+++ SpringMVC/springmvc-04-controller/src/main/java/com/T_Lyon/controller/RestFulController.java
@@ -0,0 +1,38 @@
+package com.T_Lyon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class RestFulController {
+    //1.原来的风格：http://localhost:8080/springmvc_04_controller/add?a=2&b=3
+    //@RequestMapping("/add")
+    //2.RestFul风格：http://localhost:8080/springmvc_04_controller/add/2/3
+    //@RequestMapping("/add/{a}/{b}")
+
+    //@RequestMapping(value = "/add/{a}/{b}", method = RequestMethod.GET)
+    //@RequestMapping(value = "/add/{a}/{b}", method = RequestMethod.DELETE)
+
+    @RequestMapping(value = "/hello", method = {RequestMethod.GET})
+    public String index2(Model model) {
+        model.addAttribute("msg", "hello!");
+        return "test";
+    }
+
+    //restful方式传递参数
+    @GetMapping("/add/{a}/{b}")
+    public String test1(@PathVariable int a, @PathVariable String b, Model model) {
+
+        model.addAttribute("msg", "结果为get方式：" + (a + b));
+        System.out.println("结果为get方式：" + (a + b));
+        return "test";
+    }
+
+    @PostMapping("/add/{a}/{b}")
+    public String test2(@PathVariable int a, @PathVariable int b, Model model) {
+
+        model.addAttribute("msg", "结果为post方式：" + (a + b));
+        return "test";
+    }
+}
